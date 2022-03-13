78d520ac4728d285d4bc421884d072bfead02687
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-13 19:59:55 +0700

    add horizontal category navigation

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:beauty_online_shop/constants.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
@@ -7,14 +8,40 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text(
-          "Perfum",
-          style: Theme.of(context)
-              .textTheme
-              .headline5
-              ?.copyWith(fontWeight: FontWeight.bold),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text(
+            "Perfum",
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
         )
       ],
+    );
+  }
+}
+
+class Categories extends StatefulWidget {
+  const Categories({Key? key}) : super(key: key);
+  @override
+  State<Categories> createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ['Perfume', 'Cosmetics', 'MakeUp', 'Bijoux'];
+  int _selectedCategory = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index)=>Text(categories[index]),
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:beauty_online_shop/constants.dart';
+import 'package:beauty_online_shop/screens/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,29 +9,29 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
+      body: Body(),
     );
   }
 }
 
-AppBar buildAppBar(){
-  return       AppBar(: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {},
-          color: kTextColor,
-        ),
-        actionsIconTheme: IconThemeData(
-          color: kTextColor,
-        ),
-        actions: <Widget>[
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_bag_outlined)),
-          SizedBox(
-            width: kDefaultPaddin / 2,
-          ),
-        ],
+AppBar buildAppBar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    leading: IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {},
+      color: kTextColor,
+    ),
+    actionsIconTheme: IconThemeData(
+      color: kTextColor,
+    ),
+    actions: <Widget>[
+      IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+      IconButton(onPressed: () {}, icon: Icon(Icons.shopping_bag_outlined)),
+      SizedBox(
+        width: kDefaultPaddin / 2,
       ),
-  )
+    ],
+  );
 }
