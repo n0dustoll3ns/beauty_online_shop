9531487b6a77774a2cdb7c598fb3186c22244e8f
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-13 20:42:03 +0700

    category underscale added

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -7,11 +7,12 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
           child: Text(
-            "Perfum",
+            "Beauty",
             style: Theme.of(context)
                 .textTheme
                 .headline5!
@@ -31,7 +32,7 @@ class Categories extends StatefulWidget {
 }
 
 class _CategoriesState extends State<Categories> {
-  List<String> categories = ['Perfume', 'Cosmetics', 'MakeUp', 'Bijoux'];
+  List<String> categories = ['Perfume', 'Cosmetics', 'MakeUp', 'Bijoux', '121'];
   int _selectedCategory = 0;
 
   @override
@@ -49,7 +50,21 @@ class _CategoriesState extends State<Categories> {
   Widget buildCategory(int index) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-      child: Text(categories[index]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            categories[index],
+            style: TextStyle(fontWeight: FontWeight.bold, color: kTextColor),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: kDefaultPaddin/4),//top padding
+            height: 2,
+            width: 30,
+            color: Colors.black,
+          ),
+        ],
+      ),
     );
   }
 }
