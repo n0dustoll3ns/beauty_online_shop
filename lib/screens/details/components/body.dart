90e5dafa36177f2448e870362baa58b34dd094d3
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-15 15:15:54 +0700

    Detailpage build>>

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -41,10 +41,34 @@ class Body extends StatelessWidget {
                         product.title,
                         style: Theme.of(context).textTheme.headline4!.copyWith(
                             color: Colors.white, fontWeight: FontWeight.bold),
-                      )
+                      ),
+                      Row(
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(text: 'Price\n'),
+                                TextSpan(
+                                  text: '₽${product.price}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline4!
+                                      .copyWith(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: kDefaultPaddin),
+                          Expanded(
+                            child: Image.asset(product.image),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                )
+                ),
               ],
             ),
           ),
