d90bdfcf96ed174b2cc6753b462837ceb52e1afa
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-15 13:43:10 +0700

    Detailpage Container with props

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -5,10 +5,21 @@ import 'package:beauty_online_shop/products/perfumery.dart';
 
 class Body extends StatelessWidget {
   final Perfumery product;
-  
+
   const Body({Key? key, required this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView();
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: size.height *0.3),
+          height: 500,
+          decoration: BoxDecoration(
+            color: Colors.white
+          ),
+        )
+      ]),
+    );
   }
 }
