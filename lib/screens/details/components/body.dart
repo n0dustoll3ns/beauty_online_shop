833b9534a3ae028e1471ba31c5df854048f01a52
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-15 17:55:25 +0700

    extract producttitle with image

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -28,53 +28,7 @@ class Body extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(width: kDefaultPaddin),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const Text(
-                        'Парфюмированная вода',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        product.title,
-                        maxLines: 1,
-                        overflow: TextOverflow.fade,
-                        style: Theme.of(context).textTheme.headline4!.copyWith(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(text: 'Price\n'),
-                                TextSpan(
-                                  text: '₽${product.price}',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline4!
-                                      .copyWith(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(width: kDefaultPaddin),
-                          Expanded(
-                            child: Image.asset(
-                              product.image,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
+                ProductTitleWithImage(product: product),
               ],
             ),
           ),
@@ -83,3 +37,4 @@ class Body extends StatelessWidget {
     );
   }
 }
+
