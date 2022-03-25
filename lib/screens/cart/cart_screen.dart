6fd1bc7f41a6efb36b59a22aff68b3241a7c0940
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-25 14:16:16 +0700

    add cart file and stf  widget

---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+class CartScreen extends StatefulWidget {
+  const CartScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
