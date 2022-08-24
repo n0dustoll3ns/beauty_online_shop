7d85186af313fe1d41413ae4798d897c64fa26df
n0dustoll3ns <fominykh.dg@gmail.com>
2022-08-24 22:21:18 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
-    primarySwatch: Colors.blueGrey,
+    primarySwatch: Colors.cyan,
           visualDensity: VisualDensity.adaptivePlatformDensity),
       home: const HomeScreen(),
     );
